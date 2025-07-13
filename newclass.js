d22241c99482e9e3f6f163567bef4e1e304bf3d5
MuhammadIhsan777 <oppocowo31@gmail.com>
2025-07-13 12:12:41 +0700

    menambahkan class motor, mobil, dsb..

---
+++ newclass.js
@@ -0,0 +1,31 @@
+class MotorVehicle {
+    roda;
+    bahanBakar;
+    Setir;
+    Warna;
+}
+class CarVehicle {
+    roda;
+    bahanBakar;
+    Setir;
+    Warna;
+}
+class BikeVehicle {
+    roda;
+    bahanBakar;
+    Setir;
+    Warna;
+}
+class BusVehicle {
+    roda;
+    bahanBakar;
+    Setir;
+    Warna;
+}
+class PlaneVehicle {
+    roda;
+    bahanBakar;
+    Setir;
+    Warna;
+}
+
